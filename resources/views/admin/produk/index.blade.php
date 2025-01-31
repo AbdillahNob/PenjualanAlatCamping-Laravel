943855f80b55e224f4603a9fc38e3191fe3d64df
kwangmin123455 <abdillah.kuhaku@gmail.com>
2025-02-01 00:24:51 +0800

    Buat RoleMiddleware dan rapikan web-Route

--- resources/views/admin/produk/index.blade.php
+++ resources/views/admin/produk/index.blade.php
@@ -21,24 +21,11 @@
 
                     <div class="card-body">
                         <div class="header-right">
-                            <div class="input-group icons">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-transparent border-0 pr-2 pr-sm-3"
-                                        id="basic-addon1"><i class="mdi mdi-magnify"></i></span>
-                                </div>
-                                <input type="search" class="form-control" placeholder="Cari Alat Camping"
-                                    aria-label="Cari Alat Camping">
-                                <div class="drop-down animated flipInX d-md-none">
-                                    <form action="#">
-                                        <input type="text" class="form-control" placeholder="Search">
-                                    </form>
-                                </div>
-                            </div>
+                            <a href="{{ route('create.produk') }}"><button class="btn btn-primary mb-3">Tambah
+                                    Produk</button></a>
                         </div>
 
                         <h4 class="card-title">Produk</h4>
-                        <a href="{{ route('create.produk') }}"><button class="btn btn-primary mb-3">Tambah
-                                Produk</button></a>
 
                         <div class="table-responsive">
                             <table class="table table-bordered verticle-middle">
--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,33 @@ Route::get('/logOut', 'App\Http\Controllers\LoginController@logOut')->name('logO
 Route::get('/create', 'App\Http\Controllers\Admin\UserController@create')->name('register.user');
 Route::post('/store', 'App\Http\Controllers\Admin\UserController@store')->name('store.user');
 
-// CUSTOMER
-Route::get('/produkCustomer', 'App\Http\Controllers\Customer\ProdukController@index')->name('customer.produk');
-Route::get('/tambahKeranjang{id}', 'App\Http\Controllers\Customer\KeranjangController@tambahKeranjang')->name('pesan.produk'); //Proses setelah di halaman Produk qt klik masukkan keranjang ke suatu produk
-Route::get('/keranjangCustomer', 'App\Http\Controllers\Customer\KeranjangController@index')->name('customer.keranjang'); //Tampilan Keranjang
-Route::get('/checkoutKeranjang/{id}/{idProduk}', 'App\Http\Controllers\Customer\KeranjangController@checkoutKeranjang')->name('pesan.checkout');
-Route::put('/bayarCheckout{id}', 'App\Http\Controllers\Customer\KeranjangController@bayarKeranjang')->name('bayar.checkout');
-Route::get('/riwayatCustomer', 'App\Http\Controllers\Customer\KeranjangController@riwayat')->name('customer.riwayat'); //Tampilan Keranjang
+//----------------------------------= CUSTOMER = ----------------------------------------------------------------------------
+Route::prefix('customer')->middleware(['auth', 'status:customer'])->group(function(){
+    Route::get('/produkCustomer', 'App\Http\Controllers\Customer\ProdukController@index')->name('customer.produk');
 
-// ADMIN
-Route::get('/dashboardAdmin', 'App\Http\Controllers\DashboardController@index')->name('index.dashboard');
-Route::get('/produkAdmin', 'App\Http\Controllers\Admin\ProdukController@index')->name('admin.produk');
-Route::get('/createProduk', 'App\Http\Controllers\Admin\ProdukController@create')->name('create.produk');
-Route::post('/storeProduk', 'App\Http\Controllers\Admin\ProdukController@store')->name('store.produk');
-Route::get('/editProduk/{id}', 'App\Http\Controllers\Admin\ProdukController@edit')->name('edit.produk');
-Route::put('/updateProduk/{id}', 'App\Http\Controllers\Admin\ProdukController@update')->name('update.produk');
-Route::delete('/deleteProduk/{id}', 'App\Http\Controllers\Admin\ProdukController@delete')->name('destroy.produk');
+    // KERANJANG
+    Route::get('/tambahKeranjang{id}', 'App\Http\Controllers\Customer\KeranjangController@tambahKeranjang')->name('pesan.produk'); //Proses setelah di halaman Produk qt klik masukkan keranjang ke suatu produk
+    Route::get('/keranjangCustomer', 'App\Http\Controllers\Customer\KeranjangController@index')->name('customer.keranjang'); //Tampilan Keranjang
+    Route::get('/checkoutKeranjang/{id}/{idProduk}', 'App\Http\Controllers\Customer\KeranjangController@checkoutKeranjang')->name('pesan.checkout');
+    Route::put('/bayarCheckout{id}', 'App\Http\Controllers\Customer\KeranjangController@bayarKeranjang')->name('bayar.checkout');
 
-Route::get('/dataCustomer', 'App\Http\Controllers\Admin\CustomerController@index')->name('admin.customer');
+    // RIWAYAT
+    Route::get('/riwayatCustomer', 'App\Http\Controllers\Customer\KeranjangController@riwayat')->name('customer.riwayat'); //Tampilan Keranjang
+});
 
-Route::get('/ProdukTerjual', 'App\Http\Controllers\Admin\KeranjangController@index')->name('admin.keranjang');
+// ----------------------------------------= ADMIN =-------------------------------------------------------------------------
+    Route::get('/dashboardAdmin', 'App\Http\Controllers\DashboardController@index')->name('index.dashboard');
+
+    // PRODUK
+    Route::get('/produkAdmin', 'App\Http\Controllers\Admin\ProdukController@index')->name('admin.produk');
+    Route::get('/createProduk', 'App\Http\Controllers\Admin\ProdukController@create')->name('create.produk');
+    Route::post('/storeProduk', 'App\Http\Controllers\Admin\ProdukController@store')->name('store.produk');
+    Route::get('/editProduk/{id}', 'App\Http\Controllers\Admin\ProdukController@edit')->name('edit.produk');
+    Route::put('/updateProduk/{id}', 'App\Http\Controllers\Admin\ProdukController@update')->name('update.produk');
+    Route::delete('/deleteProduk/{id}', 'App\Http\Controllers\Admin\ProdukController@delete')->name('destroy.produk');
+
+    // CUSTOMER
+    Route::get('/dataCustomer', 'App\Http\Controllers\Admin\CustomerController@index')->name('admin.customer');
+
+    // KERANJANG
+    Route::get('/ProdukTerjual', 'App\Http\Controllers\Admin\KeranjangController@index')->name('admin.keranjang');
